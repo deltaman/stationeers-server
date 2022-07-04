358f733b0606fbb9bdee6c5aeb4db9073f99ac57
deltaman <hugo@deltaman.nl>
2022-07-04 11:59:34 +0200

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -4,4 +4,4 @@
 ./docker_build.sh
 
 # Run a container
-docker run -p 0.0.0.0:27815:27815/tcp -p 0.0.0.0:27815:27815/udp -v "$(pwd)/stationeers_data:/steamcmd/stationeers" --name stationeers-server -it --rm deltaman01/stationeers-server:latest
+docker run -p 0.0.0.0:27815:27815/tcp -p 0.0.0.0:27815:27815/udp -p 0.0.0.0:27816:27816/udp -v "$(pwd)/stationeers_data:/steamcmd/stationeers" --name stationeers-server -it --rm deltaman01/stationeers-server:latest
